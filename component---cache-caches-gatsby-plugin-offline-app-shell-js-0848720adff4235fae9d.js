a37f5c0d3a1730b7f6df602983bb95a604857bbb
Fa Kin Yiu, Franklin <lifeaskrystal@hotmail.com>
2025-04-10 18:30:08 -0600

    Add public folder for GitHub Pages

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-0848720adff4235fae9d.js
@@ -0,0 +1,12 @@
+"use strict";
+(self["webpackChunkgatsby_starter_hello_world"] = self["webpackChunkgatsby_starter_hello_world"] || []).push([[681],{
+
+/***/ 7541:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+var _interopRequireDefault=__webpack_require__(4994);exports.__esModule=true;exports["default"]=void 0;var _inheritsLoose2=_interopRequireDefault(__webpack_require__(6221));var _react=_interopRequireDefault(__webpack_require__(6540));var AppShell=/*#__PURE__*/function(_React$Component){(0,_inheritsLoose2.default)(AppShell,_React$Component);function AppShell(){return _React$Component.apply(this,arguments)||this;}var _proto=AppShell.prototype;_proto.render=function render(){return/*#__PURE__*/_react.default.createElement(_react.default.Fragment,null);};return AppShell;}(_react.default.Component);var _default=AppShell;exports["default"]=_default;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-0848720adff4235fae9d.js.map
